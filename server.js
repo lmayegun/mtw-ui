6df55cea6d1e29a6d5bd4dce8748e7f7d4321fca
Lukmon Mayegun <lukmonmayegun@ML-C02G60KSML7L.bigbluedoor.net>
2022-05-03 04:13:55 +0100

    add cache middle function

--- server.js
+++ server.js
@@ -11,15 +11,41 @@ const randomInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
+const verifyCache = (req, res, next) => {
+  try {
+    const { limit } = req.query;
+    if (cache.has(limit)) {
+      return res.status(200).json(cache.get(limit));
+    }
+    return next();
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
 app.use(responseTime((req, res, time) => {
   console.log('response time is:');
   console.log(`${req.method} ${req.url} ${time}`);
 }));
 
-app.get('/images', ({ query }, res) => {
+app.get('/images', verifyCache, ({ query }, res) => {
   const i = (query.limit) ? images.slice(0, parseInt(query.limit)) : images;
+  let data;
+  try{
+    cache.set(i.length, i);
+  } catch(e){
+    console.log(e);
+  }
 
+  // return cached data 
+  if (cache.get(i.length)) {
+    return res.status(200).json(cache.get(i.length));
+  }
+
+  // else get new data.
   setTimeout(() => {
+    data = res.status(200).json(i);
+    cache.set(i.length, i);
     return res.status(200).json(i);
   }, randomInterval(500, 1500));
 });
