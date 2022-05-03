6d3d5c61d7d1259cb5f6220026de3bde6d32095a
Lukmon Mayegun <lukmonmayegun@ML-C02G60KSML7L.bigbluedoor.net>
2022-05-03 03:54:18 +0100

    add lightbox effect

--- src/components/CardImg.js
+++ src/components/CardImg.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Lightbox } from "react-modal-image";
 
 const CardImg = ({imgData}) => {
+  const [toggleModal, setToggleModal] = useState(false);
   const {imgUrl, profileImg, altDescr} = imgData;
   return (
     <>
-      <div className={'wrap'}>
+      <div className={'wrap'} onClick={()=>{setToggleModal(true)}}>
         <img src={imgUrl} alt={altDescr} className={'card_img'}/>
         <img src={profileImg} alt={altDescr} className={'profile_img'}/>
       </div>
+
+      {toggleModal && (
+        <Lightbox
+          medium={imgUrl}
+          large={imgUrl}
+          onClose={(prev)=>{
+            setToggleModal(false)
+          }}
+        />
+        )
+      }
+
     </>
   );
 }
