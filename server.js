90a60a3a08b20c11d44cc6f9e6c3505a6f4094f5
Lukmon Mayegun <lukmonmayegun@ML-C02G60KSML7L.bigbluedoor.net>
2022-05-03 04:10:33 +0100

    log out response time

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 'use strict';
 
 const app = require('express')();
+const responseTime = require('response-time');
+const NodeCache = require("node-cache");
 const images = require('./src/images.json');
+
+const cache = new NodeCache({ stdTTL: 15 });
 
 const randomInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+app.use(responseTime((req, res, time) => {
+  console.log('response time is:');
+  console.log(`${req.method} ${req.url} ${time}`);
+}));
 
 app.get('/images', ({ query }, res) => {
   const i = (query.limit) ? images.slice(0, parseInt(query.limit)) : images;
